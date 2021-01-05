8afd45c52934d84dd258628de5bb850fa58ff124
aileron33 <funpromotion.apps@gmail.com>
2021-01-05 15:19:04 +0900

    master_list完成！

--- selenium_loto6.py
+++ selenium_loto6.py
@@ -10,7 +10,7 @@ options.add_argument('--incognito')  #シークレットモードを使う
 driver = webdriver.Chrome(executable_path=chrome_path, options=options) 
 
 #driverでurlにアクセスする----------------------------------------------------------------------
-url = 'https://www.mizuhobank.co.jp/retail/takarakuji/loto/loto6/index.html?year=2020&month=10'
+url = 'https://www.mizuhobank.co.jp/retail/takarakuji/loto/loto6/index.html?year=2020&month=11'
 driver.get(url)
 
 #loto6にアクセスして、キーワード入力、閉じるところまで--------------------------------------
@@ -20,7 +20,7 @@ helds = driver.find_elements_by_xpath("//th[@class='alnCenter bgf7f7f7 js-lotter
 sleep(3)
 date = driver.find_elements_by_xpath("//td[@class='alnCenter js-lottery-date-pc']") #日付
 sleep(3)
-number = driver.find_elements_by_xpath("//td[@class='alnCenter extension']") #数字
+numbers = driver.find_elements_by_xpath("//td[@class='alnCenter extension']") #数字
 sleep(3)
 
 
@@ -28,57 +28,63 @@ sleep(3)
 #取り出してtextにせいや---＞
 print('開催回数' + str(len(helds))) #9個ある
 print('開催日数' + str(len(date))) #9個ある
-print('数字総数' + str(len(number)))  #54個ある
+print('数字総数' + str(len(numbers)))  #54個ある
 
 held_list=[]
 date_list = []
-dict_list = []
+number_list = []
+master_list=[]
 
-#date日付からtextを取り出してリストに入れる----------------------------------------------------------------------------
+#date日付からtextを取り出してheldsリストに入れる----------------------------------------------------------------------------
 for held in helds:
     held_list.append(held.text)
+#-->print(held_list)
 
-print(held_list)
-
-#date日付からtextを取り出してリストに入れる----------------------------------------------------------------------------
+#date日付からtextを取り出してdateリストに入れる----------------------------------------------------------------------------
 for day in date:
     date_list.append(day.text)
+#-->print(date_list)
 
-print(date_list)
-
-#number配列メモリ番地からtextを取り出す---len(number)=54個----------------------6個づつ---------------------------------------
-for element in number:
-    dict_list.append(element.text)
+#number配列メモリ番地からtextを取り出してnumberリストに入れる--------------------6個づつ---------------------------------------
+for number in numbers:
+    number_list.append(number.text)
 
 #開始位置を指定
 n = 0
 #分割する変数の個数を指定
 s = 6
 
-#変数名カウント（ループ回数）
-count=1
-
 #s個づつリストを分割する。
-for i in dict_list:
-    locals()['name' + str(n + 1)] = dict_list[n: n + s :1]
-    print('name' + str(count))
-    print(dict_list[n: n + s :1])
-    count += 1
-    n += s
-    
-    #カウント数が配列の長さを超えたらループ終了
-    if n >= len(dict_list):
-        count=1
+for i in number_list:
+    master_list.append(number_list[n:n+s:1])  #<--配列にappendしてしまう（丸ごと）
+    n += s #<-6回ループ
+                    
+    if n >= len(number_list):  #<-カウント数が配列の長さを超えたらループ終了
         break
 
-print('dict_list総数' + str(len(dict_list)))
-#print(dict_list)
+#-----------------------------------------------------------配列頭に日付を追加
+for i,day in enumerate(date_list):
+    master_list[i].insert(0,day)
+
+#-----------------------------------------------------------配列頭に回数を追加
+for i,held in enumerate(held_list):
+    master_list[i].insert(0,held)
+
+print(master_list) 
+
 
 #クローズ------------------------------------------------------------------------
 driver.close()
 driver.quit()
 #ここまで可能
 print('処理終了')
+
+
+#-----------------------------------------------------------------------------ここからcsvに出力
+
+
+
+
 
 
 #（注意）Driver閉じてから、処理を置くとNG->「 Failed to establish a new connection: [WinError 10061] 対象のコンピューターによって拒否されたため、接続できませんでした。」
